9d094ac94ec84392f2077fabba57f49746906eaf
unipeso_baiyuheng <864834604@qq.com>
2019-06-03 16:20:45 +0800

    test3 and add contact.py

--- test.py
+++ test.py
@@ -1,16 +1,18 @@
 print('hello world')
 print('ok')
 
-
 import pandas as pd
+
 
 def a():
     print('a')
     return True
 
+
 def b():
     print("b")
     return False
+
 
 if a() or b():
     print('or')
